e6550c13a6e1bb2264a98c116ea0197d3e185dd7
Andrew Kane <acekane1@gmail.com>
2017-05-01 00:34:41 -0700

    Updated belongs_to relation

--- app/models/notable/request.rb
+++ app/models/notable/request.rb
@@ -2,7 +2,7 @@ module Notable
   class Request < ActiveRecord::Base
     self.table_name = "notable_requests"
 
-    belongs_to :user, polymorphic: true
+    belongs_to :user, {polymorphic: true}.merge(ActiveRecord::VERSION::MAJOR >= 5 ? {optional: true} : {})
     serialize :params, JSON
   end
 end
