fb52edb1ec5abce08f3f378dfcb015920fd7f624
Andrew Kane <andrew@chartkick.com>
2019-05-28 22:06:33 -0700

    No need for variable

--- lib/notable/job_extensions.rb
+++ lib/notable/job_extensions.rb
@@ -5,8 +5,7 @@ module Notable
     included do
       around_perform do |job, block|
         # no way to get queued_at time :(
-        slow_job_threshold = try(:notable_slow_job_threshold)
-        Notable.track_job(job.class.name, job.job_id, job.queue_name, nil, slow_job_threshold) do
+        Notable.track_job(job.class.name, job.job_id, job.queue_name, nil, try(:notable_slow_job_threshold)) do
           block.call
         end
       end
