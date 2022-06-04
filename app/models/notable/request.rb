0601992162a78d8f130ad15807985e97751cf353
van.tran <itlouistran@gmmail.com>
2022-06-05 01:17:19 +0700

    fix issue when params is too long

--- app/models/notable/request.rb
+++ app/models/notable/request.rb
@@ -4,5 +4,11 @@ module Notable
 
     belongs_to :user, polymorphic: true, optional: true
     serialize :params, JSON
+
+    before_save :truncate_too_long_params
+
+    def truncate_too_long_params
+      params = JSON.parse(params.to_s.gsub(/\"data:.+\/.+;base64.+\"/, 'base64_image'))
+    end
   end
 end
