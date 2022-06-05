e540e23d3aba2b177f24844633d04e0ab767b52f
van.tran <itlouistran@gmmail.com>
2022-06-05 15:05:06 +0700

    update base64 regex

--- app/models/notable/request.rb
+++ app/models/notable/request.rb
@@ -8,7 +8,8 @@ module Notable
     before_save :truncate_too_long_params
 
     def truncate_too_long_params
-      params = JSON.parse(params.to_s.gsub(/\"data:.+\/.+;base64.+\"/, 'base64_image'))
+      base64_regex = /(\"data\:)(\w+\/\w+)(\;base64\,).+?(\")/
+      self.params = JSON.parse(self.params.to_json.to_s.gsub(base64_regex, '\1\2\3...\4'))
     end
   end
 end
