9ff8b4e17853ae0fc19d661576f98df9b513885e
Andrew Kane <andrew@ankane.org>
2026-04-01 19:35:23 -0700

    Removed unneeded code [skip ci]

--- lib/notable/job_extensions.rb
+++ lib/notable/job_extensions.rb
@@ -4,8 +4,7 @@ module Notable
 
     included do
       around_perform do |job, block|
-        # enqueued_at is only available in Active Job 6+
-        Notable.track_job(job.class.name, job.job_id, job.queue_name, job.try(:enqueued_at), try(:notable_slow_job_threshold)) do
+        Notable.track_job(job.class.name, job.job_id, job.queue_name, job.enqueued_at, try(:notable_slow_job_threshold)) do
           block.call
         end
       end
